098039025b1f1fbe76b81ca48dfe7434040fb1b1
Andrew James Cavanagh <acavanagh@gmail.com>
2016-08-27 17:12:18 -0700

    Initial Commit

---
+++ Mojave/Mojave.h
@@ -0,0 +1,19 @@
+//
+//  Mojave.h
+//  Mojave
+//
+//  Created by Andrew Cavanagh on 8/27/16.
+//  Copyright © 2016 Andrew Cavanagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mojave.
+FOUNDATION_EXPORT double MojaveVersionNumber;
+
+//! Project version string for Mojave.
+FOUNDATION_EXPORT const unsigned char MojaveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mojave/PublicHeader.h>
+
+
